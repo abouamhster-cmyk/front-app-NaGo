08bd936604a995e60e44d72fdf9c87427c4d055f
abouamhster-cmyk <abouamhster@gmail.com>
2026-07-21 00:43:44 +0200

    Update ride_bloc.dart

--- lib/features/client_ride/presentation/bloc/ride_bloc.dart
+++ lib/features/client_ride/presentation/bloc/ride_bloc.dart
@@ -1,14 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/network/websocket_client.dart';
 import '../../data/ride_service.dart';
 import 'ride_event.dart';
 import 'ride_state.dart';
 
 class RideBloc extends Bloc<RideEvent, RideState> {
   final RideService _rideService;
+  final WebSocketClient _webSocketClient;
 
-  RideBloc(this._rideService) : super(RideInitialState()) {
+  RideBloc(this._rideService, this._webSocketClient) : super(RideInitialState()) {
+    
+    // NOUVEAU : Écouter en direct si le prestataire déclare avoir reçu les espèces
+    _webSocketClient.onEvent('cash_payment_declared', (data) {
+      add(OnCashPaymentDeclaredEvent(data as Map<String, dynamic>));
+    });
 
-    // 1. Récupérer les conducteurs proches
     on<GetNearbyDriversEvent>((event, emit) async {
       emit(RideLoadingState());
       try {
@@ -22,7 +28,6 @@ class RideBloc extends Bloc<RideEvent, RideState> {
       }
     });
 
-    // 2. Lancer la réservation d'un trajet ou d'une livraison
     on<RequestRideEvent>((event, emit) async {
       emit(RideLoadingState());
       try {
@@ -41,5 +46,17 @@ class RideBloc extends Bloc<RideEvent, RideState> {
         emit(RideFailureState(e.toString()));
       }
     });
+
+    // NOUVEAU : Traiter l'événement de déclaration d'espèces reçu en direct
+    on<OnCashPaymentDeclaredEvent>((event, emit) {
+      emit(CashPaymentValidationRequiredState(event.paymentDetails));
+    });
+  }
+
+  @override
+  Future<void> close() {
+    // Libérer l'écouteur WebSocket pour éviter les fuites de mémoire
+    _webSocketClient.offEvent('cash_payment_declared');
+    return super.close();
   }
 }
