06fdcb5f2ee060f9e125dd9ffa54c91b9612cc91
abouamhster-cmyk <abouamhster@gmail.com>
2026-07-20 23:42:24 +0200

    Create ride_event.dart

---
+++ lib/features/client_ride/presentation/bloc/ride_event.dart
@@ -0,0 +1,31 @@
+abstract class RideEvent {}
+
+// Action de chercher les conducteurs autour de la position actuelle du client
+class GetNearbyDriversEvent extends RideEvent {
+  final double latitude;
+  final double longitude;
+  GetNearbyDriversEvent(this.latitude, this.longitude);
+}
+
+// Action de lancer la réservation
+class RequestRideEvent extends RideEvent {
+  final String typeService;
+  final String adresseDepart;
+  final double latitudeDepart;
+  final double longitudeDepart;
+  final String adresseArrivee;
+  final double latitudeArrivee;
+  final double longitudeArrivee;
+  final double prixPropose;
+
+  RequestRideEvent({
+    required this.typeService,
+    required this.adresseDepart,
+    required this.latitudeDepart,
+    required this.longitudeDepart,
+    required this.adresseArrivee,
+    required this.latitudeArrivee,
+    required this.longitudeArrivee,
+    required this.prixPropose,
+  });
+}
